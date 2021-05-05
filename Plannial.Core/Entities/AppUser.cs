e970f1d8e64457c3a152607fc2b1b06b823f80e3
chrisk <christian.khalil200@gmail.com>
2021-05-05 19:26:47 +0200

    messages

--- Plannial.Core/Entities/AppUser.cs
+++ Plannial.Core/Entities/AppUser.cs
@@ -5,9 +5,10 @@ namespace Plannial.Core.Entities
     public class AppUser
     {
         public string Id { get; set; }
-        public string UserName { get; set; }
+        public string Email { get; set; }
         public string Password { get; set; }
         public ICollection<Subject> Subjects { get; set; } = new List<Subject>();
         public ICollection<Reminder> Reminders { get; set; } = new List<Reminder>();
+        public ICollection<Message> Messages { get; set; } = new List<Message>();
     }
 }
---
+++ Plannial.Core/Entities/Message.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Plannial.Core.Entities
+{
+    public class Message
+    {
+        public int Id { get; set; }
+        public int RecipientId { get; set; }
+        public int SenderId { get; set; }
+        public string Content { get; set; }
+        public DateTime DateSent { get; set; } = DateTime.UtcNow;
+        public DateTime? DateRead { get; set; }
+    }
+}
