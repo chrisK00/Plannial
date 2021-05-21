a8c460775fde2a6390ab02fa1bbb21c6725ba1e8
chrisk <christian.khalil200@gmail.com>
2021-05-21 10:12:35 +0200

    auth service

---
+++ Plannial-Client/src/app/_services/auth.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { ReplaySubject } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
+import { User } from '../_models/user';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private authUrl = environment.baseUrl + 'account';
+  private currentUser = new ReplaySubject<User>(1);
+  currentUser$ = this.currentUser.asObservable();
+
+  constructor(private http: HttpClient) { }
+
+  login(request: any) {
+    return this.http.post(`${this.authUrl}login`, request).pipe(
+      map((user: User) => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.setCurrentUser(user);
+        }
+      }));
+  }
+
+  setCurrentUser(user: User) {
+    this.currentUser.next(user);
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.next(undefined);
+  }
+}
+
