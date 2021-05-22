5c9dc4c2cddd7b950997778076594f732bf17712
chrisk <christian.khalil200@gmail.com>
2021-05-22 09:47:43 +0200

    send back user usernames when getting messages

--- Plannial.Core/Queries/GetMessages.cs
+++ Plannial.Core/Queries/GetMessages.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
 using MediatR;
@@ -15,15 +16,28 @@ namespace Plannial.Core.Queries
         public class Handler : IRequestHandler<Query, IEnumerable<MessageResponse>>
         {
             private readonly IMessageRepository _messageRepository;
+            private readonly IUserRepository _userRepository;
 
-            public Handler(IMessageRepository messageRepository)
+            public Handler(IMessageRepository messageRepository, IUserRepository userRepository)
             {
                 _messageRepository = messageRepository;
+                _userRepository = userRepository;
             }
 
             public async Task<IEnumerable<MessageResponse>> Handle(Query request, CancellationToken cancellationToken)
             {
-                return await _messageRepository.GetMessagesAsync(request.UserId, request.MessageParams, cancellationToken);
+                var messages = await _messageRepository.GetMessagesAsync(request.UserId, request.MessageParams, cancellationToken);
+
+                foreach (var message in messages)
+                {
+                    var sender = await _userRepository.GetUserAsync(message.SenderId);
+                    var recipient = await _userRepository.GetUserAsync(message.RecipientId);
+
+                    message.SenderUsername = sender.UserName;
+                    message.RecipientUsername = recipient.UserName;
+                }
+
+                return messages;
             }
         }
     }
